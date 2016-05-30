2a211c6a1d289f20f33b5900780053fa973c7400
manidf <mannuel.ferreira@gmail.com>
2016-05-30 23:46:35 +0200

    test for ProductList created

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,13 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
 
 Route::group(['prefix' => 'api'], function () {
 
-		Route::get('products', ['as' => 'products', function () {
-				return App\Product::all();
-		}]);
+	Route::get('products', ['as' => 'products', function () {
+		return App\Product::all();
+	}]);
 
 });
