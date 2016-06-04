411d6e686d4f8cb04959d6a4da0cc7e2b293e002
manidf <mannuel.ferreira@gmail.com>
2016-06-04 06:42:52 +0200

    update route

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,7 +10,7 @@ class ProductController extends Controller
 {
 		/**
 		 * Display a listing of the ResourceBundle
-		 * @return REsponse
+		 * @return Response
 		 **/
 		 public function index()
 		 {
